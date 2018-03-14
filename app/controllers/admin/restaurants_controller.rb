2b3f057e853d8084e449505c844b9e955701f53e
HazelPan <sea39801991@gmail.com>
2018-03-14 17:50:33 +0800

    add restaurant edit and update actions and views & destroy action for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
@@ -22,15 +23,35 @@ class Admin::RestaurantsController < ApplicationController
 
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
+  def update
+    
+    if @restaurant.update(restaurant_params)
+      flash[:alert] = "restaurant was successfully updated"
+      redirect_to admin_restaurant_path(@restaurant)
+    else
+      flash[:alert] = "restaurant was failed to update"
+      render :edit
+    end
   end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to :index
+    flash[:alert] = "restaurant was deleted"
+  end
+
+
 
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    
   end
 
 
