dcd7f95433f371aa61c77ec2e56b86de66e57f1f
HazelPan <sea39801991@gmail.com>
2018-04-01 23:33:45 +0800

    user can view commented restaurants on profile page & styled profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
-
   mount_uploader :avatar, AvatarUploader
 
-  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
+  has_many :comments
+  has_many :restaurants, through: :comments
 
+  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"
   end
+
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -4,7 +4,7 @@ namespace :dev do
 
     500.times do |i|
       Restaurant.create!(name: FFaker::Name.first_name,
-        opening_hours: FFaker::Time.datetime,
+        open_hours: FFaker::Time.datetime,
         tel: FFaker::PhoneNumber.short_phone_number,
         address: FFaker::Address.street_address,
         description: FFaker::Lorem.paragraph,
